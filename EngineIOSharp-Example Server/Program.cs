1a15ad0903004d7d99dde5e461366126250dd800
uhm0311 <uhm0311@naver.com>
2020-03-22 16:08:23 +0900

    Polish it.

--- EngineIOSharp-Example Server/Program.cs
+++ EngineIOSharp-Example Server/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EngineIOSharp.Common;
 using EngineIOSharp.Server;
-using EngineIOSharp.Common;
+using System;
 
 namespace EngineIOSharp.Example.Server
 {
